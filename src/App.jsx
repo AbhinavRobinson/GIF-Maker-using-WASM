5c71e80b73b55bafb259f8a6e4cedca0eb6012f2
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-11-22 12:39:42 +0530

    📖(chore)#4: added descriptions for functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+/**
+ * @description import ffmpeg libraries
+ */
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+
+/**
+ * @description log initialization to console
+ */
 const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
 
+  /**
+   * @description prepare flags for ready state
+   */
   const [ready, setReady] = useState(false);
 
+  /**
+   * @description load : async load ffmpeg when needed over CDN
+   */
   const load = async () => {
     await ffmpeg.load();
+    /**
+     * @description set flag true for ready once loaded
+     */
     setReady(true);
   }
+
+  /**
+   * @description loads ffmpeg
+   */
+  useEffect(() => {
+    load();
+  }, []
+   /**
+   * @description empty array at end, so function only runs once.
+   */)
 
   return (
     <div className="App">
